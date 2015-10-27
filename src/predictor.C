6ed405be9b0949d08891caff7fa8f4b708673971
will_zegers <will.zegers@gmail.com>
2015-10-27 15:12:10 -0700

    gshare predictor

--- src/predictor.C
+++ src/predictor.C
@@ -1,14 +1,38 @@
-// A very stupid predictor.  It will always predict not taken.
+#include "stdint.h"
+#include "math.h"
+
+#define TAKEN     true
+#define NOT_TAKEN false
+
+#define MEM_BUDGET  15 // 32KB (2^15)
+
+const static short bhtSize = pow(2,MEM_BUDGET - 2);
+unsigned short BHT[bhtSize];
+unsigned int GHR;
+unsigned int index;
 
 void init_predictor ()
 {
+    for (int i = 0; i < bhtSize; i++) {
+        BHT[i] = 0;
+    }
+    GHR = 0;
 }
 
 bool make_prediction (unsigned int pc)
 {
-  return false;
+    index = pc ^ GHR;
+    index %= bhtSize;
+    return (BHT[index] > 1);
 }
 
 void train_predictor (unsigned int pc, bool outcome)
 {
+    GHR <<= 1;
+    if (outcome == TAKEN && BHT[index] < 3) {
+        BHT[index]++;
+        GHR++;
+    } else if (outcome == NOT_TAKEN && BHT[index] > 0) {
+        BHT[index]--;      
+    }
 }
