4c629f6c1f3c9f82993486745f206db3bf74dc1d
will_zegers <will.zegers@gmail.com>
2015-10-27 17:44:36 -0700

    Changed variable name, added shell script to run all traces

--- src/predictor.C
+++ src/predictor.C
@@ -7,14 +7,14 @@
 #define MEM_BUDGET  15 // 32KB (2^15)
 
 const static short bhtSize = pow(2,MEM_BUDGET - 2);
-unsigned short BHT[bhtSize];
+unsigned short bht[bhtSize];
 unsigned int GHR;
 unsigned int index;
 
 void init_predictor ()
 {
     for (int i = 0; i < bhtSize; i++) {
-        BHT[i] = 0;
+        bht[i] = 0;
     }
     GHR = 0;
 }
@@ -23,16 +23,16 @@ bool make_prediction (unsigned int pc)
 {
     index = pc ^ GHR;
     index %= bhtSize;
-    return (BHT[index] > 1);
+    return (bht[index] > 1);
 }
 
 void train_predictor (unsigned int pc, bool outcome)
 {
     GHR <<= 1;
-    if (outcome == TAKEN && BHT[index] < 3) {
-        BHT[index]++;
+    if (outcome == TAKEN && bht[index] < 3) {
+        bht[index]++;
         GHR++;
-    } else if (outcome == NOT_TAKEN && BHT[index] > 0) {
-        BHT[index]--;      
+    } else if (outcome == NOT_TAKEN && bht[index] > 0) {
+        bht[index]--;      
     }
 }
