3f7636df838d46370a311a945515d9216e70e1b1
will_zegers <will.zegers@gmail.com>
2015-10-27 23:50:36 -0700

    Small changes to predictor.C and run_predictor script

--- src/predictor.C
+++ src/predictor.C
@@ -1,12 +1,12 @@
-#include "math.h"
-#include "stdlib.h"
+//#include "math.h"
 
 #define TAKEN     true
 #define NOT_TAKEN false
 #define BUDGET    32768 // (32Kb)
 
 const static int historyLength = 32;
-const static float theta = 1.93 * historyLength + 14;
+const static float theta = historyLength/2; //1.93 * historyLength + 14;
+//const static float theta = 1.93 * historyLength + 14;
 const static int tableSize = 128;
 //const static int tableSize = pow(2,floor(log2(BUDGET/(historyLength * 8))));
 
@@ -32,7 +32,7 @@ bool make_prediction (unsigned int pc)
     index = pc % tableSize;
     for(int i = 0; i < historyLength; i++)
     {
-        y += (int)W[index][i] * (H >> (31 - i) & 1); 
+        y += W[index][i] * (H >> (31 - i) & 1); 
     }
 
     bool predict = (y > 0) ? TAKEN : NOT_TAKEN;    
@@ -41,11 +41,11 @@ bool make_prediction (unsigned int pc)
 
 void train_predictor (unsigned int pc, bool outcome)
 {
-    int t = outcome ? 1 : -1;
+    int t = (outcome == TAKEN) ? 1 : -1;
 
     for(int i = 0; i < historyLength; i++)
     {
-        if(((y < 0) != (t < 0)) || abs(y) < theta)
+        if(((y < 0) != (t < 0)) || (y < theta && y > -theta))
         {
             W[index][i] += (H >> (31 - i) & 1)*t;
         }           
