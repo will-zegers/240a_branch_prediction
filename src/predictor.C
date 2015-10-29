1ed8c57061f798a20d3314c2a0b15caf2ad181e8
will_zegers <will.zegers@gmail.com>
2015-10-29 09:54:05 -0700

    First commit

--- src/predictor.C
+++ src/predictor.C
@@ -4,53 +4,42 @@
 #define NOT_TAKEN false
 #define BUDGET    32768 // (32Kb)
 
-const static int h = 31;
-
-//const static float theta = 1.93 * h + 14;
-//const static int n = pow(2,floor(log2(BUDGET/(h * 8))));
+const static int h = 18;
+const static int n = 18;
+const static int b = 8;
 const static float theta = h/2; 
-const static int n = 128;
+const static int min_weight = -(1 << (b-1));
+const static int max_weight = -min_weight - 1;
 
-char W[n][h+1];
-int H;
+bool G[h+1];
+bool SG[h+1];
+int W[n][h+1];
+int R[h+1];
+int SR[h+1];
+int v[h+1];
+int sv[h+1];
 
 int index;
 int y;
 
 void init_predictor ()
 {
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < h; j++)
+    for(int i = 0; i < h+1; i++) {
+        G[i]  = false;
+        SG[i] = false;
+        R[i]  = 0;
+        SR[i] = 0;
+        v[i]  = 0;
+        sv[i] = 0;
+        for(int j = 0; j < n; j++)
             W[i][j] = 0;
-
-    H = 0;
 }
 
 bool make_prediction (unsigned int pc)
 {
-    index = pc % n;
-
-    y = W[index][0];
-    for(int i = 1; i < h+1; i++)
-    {
-        y += W[index][i] * (H >> (h - i) & 1); 
-    }
-
-    return (y > 0) ? TAKEN : NOT_TAKEN;
+    return false;
 }
 
 void train_predictor (unsigned int pc, bool outcome)
 {
-    int t = (outcome == TAKEN) ? 1 : -1;
-
-    if(((y < 0) != (t < 0)) || (y < theta && y > -theta))
-    {
-        W[index][0] += t;
-        for(int i = 1; i < h+1; i++)
-        {
-            W[index][i] += (H >> (h - i) & 1)*t;
-        }           
-    }
-    H <<= 1;
-    H += (int)outcome;
 }
